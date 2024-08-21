44f98be8f9d94cdb566704dfb3bf513dee0ad817
g41797 <g41797@gmail.com>
2024-08-21 10:05:36 +0300

    Add zig-datetime repo as subfolder

--- build.zig
+++ build.zig
@@ -29,7 +29,6 @@ pub fn build(b: *std.Build) void {
     // running `zig build`).
     b.installArtifact(lib);
 
-
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const lib_unit_tests = b.addTest(.{
--- src/root.zig
+++ src/root.zig
@@ -1,11 +1,14 @@
 //---------------------------------
-const std               =  @import("std");
+const std               = @import("std");
 const testing           = std.testing;
 
 pub const rfc5424       = @import("rfc5424.zig");
 pub const shortstring   = @import("shortstring.zig");
 pub const pid           = @import("pid.zig");
 pub const application   = @import("application.zig");
+
+pub const datetime      = @import("./zig-datetime/src/datetime.zig");
+pub const timezones     = @import("./zig-datetime/src/timezones.zig");
 //---------------------------------
 
 test {
